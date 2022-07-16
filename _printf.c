4285cdb3a7fa6236333b489c93bf08d2aad9b7d8
abdallahahmed18 <abdallaahmedabdash@gmail.com>
2022-07-15 18:44:20 -0700

    fixing bugs in all tasks

--- _printf.c
+++ _printf.c
@@ -1,48 +1,46 @@
 #include "main.h"
-
 /**
- * _printf - produces output according to a format
- * @format: format string containing the characters and the specifiers
- * Description: this function will call the get_print() function that will
- * determine which printing function to call depending on the conversion
- * specifiers contained into fmt
- * Return: length of the formatted output string
+ * _printf - prints out a formatted string
+ * @format: the string to be formatted
+ * Return: the number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	    int (*pfunc)(va_list, flags_t *);
-	        const char *p;
-		    va_list arguments;
-		        flags_t flags = {0, 0, 0};
+	va_list list;
+	unsigned int i = 0, count = 0;
 
-			    register int count = 0;
+	va_start(list, format);
 
-			        va_start(arguments, format);
-				    if (!format || (format[0] == '%' && !format[1]))
-					            return (-1);
-				        if (format[0] == '%' && format[1] == ' ' && !format[2])
-						        return (-1);
-					    for (p = format; *p; p++)
-						        {
-								        if (*p == '%')
-										        {
-												            p++;
-													                if (*p == '%')
-																            {
-																		                    count += _putchar('%');
-																				                    continue;
-																						                }
-															            while (get_flag(*p, &flags))
-																	                    p++;
-																                pfunc = get_print(*p);
-																		            count += (pfunc)
-																				                             ? pfunc(arguments, &flags)
-																							                              : _printf("%%%c", *p);
-																			            }
-									        else
-											            count += _putchar(*p);
-										    }
-					        _putchar(-1);
-						    va_end(arguments);
-						        return (count);
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '%')
+			{
+				_putchar('%');
+				count++;
+				i++;
+			}
+			else if (parser(format, i + 1) != NULL)
+			{
+				count += parser(format, i + 1)(list);
+				i++;
+			}
+			else
+			{
+				_putchar(format[i]);
+				count++;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
+	va_end(list);
+	return (count);
 }
